538a366590aa9170f5fc5e94f912864b5afa0ad4
Damian Zabagło <damian_zabaglo@epam.com>
2025-10-09 22:42:26 +0200

    Add startTest function, add endTest function

---
+++ Lab8 - Text Analysis Tool for Speed Testing/speed_analysis.js
@@ -0,0 +1,39 @@
+let testText = "The quick brown fox jumps over the lazy dog";
+
+let startTime, endTime;
+
+function startTest(){
+    document.getElementById('inputText').value = testText;
+    let userInput = document.getElementById("userInput");
+
+    userInput.value = "";
+    userInput.readOnly = false;
+    userInput.focus();
+
+    document.getElementById('output').innerHTML = "";
+
+    startTime = new Date().getTime();
+}
+
+function endTest(){
+    endTime = new Date().getTime();
+
+    var timeElapsed = (endTime - startTime) / 1000;
+    var userTypedText = document.getElementById('userInput').value;
+
+    var typedWords = userTypedText.split(/\s+/).filter(function (word){
+        return word !== "";
+    }).length;
+
+    var wpm = 0;
+
+    if(timeElapsed !== 0 && !isNaN(typedWords)){
+        wpm = Math.round((typedWords/timeElapsed) * 60);
+    }
+
+    var outputDiv = document.getElementById("output");
+    outputDiv.innerHTML = `<h2>Typing Test Results: </h2>
+    <p>Words Typed: ${typedWords}</p>
+    <p>Time Elapsed: ${timeElapsed}</p>
+    <p>Words Per Minute(WPM): ${wpm}</p>`;
+}
