deeb9c5b6ef790f7053f081d983dc111b5e4a6c1
Damian Zabagło <damian_zabaglo@epam.com>
2025-10-09 22:11:38 +0200

    add prices in menu

--- Lab6 - Iteration for Restaurant Menu List/restaurant_menu.js
+++ Lab6 - Iteration for Restaurant Menu List/restaurant_menu.js
@@ -1,6 +1,6 @@
-const breakfastMenu = ['Pancakes', 'Oarmeal', 'Frittata'];
-const mainCourseMenu = ['Steak', 'Pasta', 'Salmon'];
-const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+const breakfastMenu = ['Pancakes - 12$', 'Oarmeal - 8$', 'Frittata - 4$'];
+const mainCourseMenu = ['Steak - 45$', 'Pasta - 17$', 'Salmon - 25$'];
+const dessertMenu = ['Cake - 7$', 'Ice Cream - 2$', 'Pudding - 5$', 'Fruit Salad - 11$'];
 
 const breakfastMenuItemsHTML = breakfastMenu.map( (item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
     document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
