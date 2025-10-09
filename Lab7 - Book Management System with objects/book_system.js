315a2a65d086f8ce9c19acaa41afdd508b707fd9
Damian Zabagło <damian_zabaglo@epam.com>
2025-10-09 22:25:28 +0200

    Add array books, add function AddBook, add function showBooks, add function editBook

---
+++ Lab7 - Book Management System with objects/book_system.js
@@ -0,0 +1,51 @@
+let books = []
+
+function addBook() {
+    const bookName = document.getElementById('bookName').value;
+    const authorName = document.getElementById('authorName').value;
+    const bookDescription = document.getElementById('bookDescription').value;
+    const pagesNumber = document.getElementById('pagesNumber').value;
+
+    if(bookName && authorName && bookDescription && !isNaN(pagesNumber)){
+        const book = {
+            name: bookName,
+            author: authorName,
+            description: bookDescription,
+            pagesNumber: pagesNumber
+        };
+        books.push(book);
+        showbooks();
+        clearInputs();
+    }
+    else{
+        alert("Please fill all fields correctly.");
+    }
+}
+
+function showbooks(){
+    const booksDiv = books.map( (book, index) => `<h1>Book Number: ${index + 1}</h1>
+    <p><strong>Book Name: </strong>${book.name}</p>
+    <p><strong>Author Name: </strong>${book.authorName}</p>
+    <p><strong>Description: </strong>${book.description}</p>
+    <p><strong>Number of pages: </strong>${book.pagesNumber}</p>
+    <button onclick="editbook(${index})">Edit</button>`
+    );
+    document.getElementById('books').innerHTML = booksDiv.join('');
+}
+
+function editBook(index){
+    const book = books[index];
+    document.getElementById('bookName').value = book.name;
+    document.getElementById('authorName').value = book.authorName;
+    document.getElementById('bookDescription').value = book.bookDescription;
+    document.getElementById('pagesNumber').value = book.pagesNumber;
+    books.splice(index, 1)
+    showbooks();
+}
+
+function clearInputs(){
+    document.getElementById('bookName').value = '';
+    document.getElementById('authorName').value = '';
+    document.getElementById('bookDescription').value = '';
+    document.getElementById('pagesNumber').value = '';
+}
