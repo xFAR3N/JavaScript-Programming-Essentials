8edd05c99dc42f52896b4c6856cff7e5d163f7f0
Damian Zabagło <damian_zabaglo@epam.com>
2025-10-09 22:09:47 +0200

    add displaying main course menu and dessert menu in .js filee

--- Lab6 - Iteration for Restaurant Menu List/restaurant_menu.js
+++ Lab6 - Iteration for Restaurant Menu List/restaurant_menu.js
@@ -3,4 +3,19 @@ const mainCourseMenu = ['Steak', 'Pasta', 'Salmon'];
 const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
 
 const breakfastMenuItemsHTML = breakfastMenu.map( (item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
-    document.getElementById('breakfastMenu').innerHTML = breakfastMenuItemsHTML;
+    document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
+
+
+let mainCourseItem = '';
+
+mainCourseMenu.forEach((item, index) => {
+    mainCourseItem += `<p>Item ${index + 1}: ${item}</p>`;
+});
+document.getElementById('maincourseMenuItems').innerHTML = mainCourseItem;
+
+let dessrtItem = '';
+
+for(let i = 0; i < dessertMenu.length; i++){
+    dessrtItem += `<p> Item ${i + 1}: ${dessertMenu[i]}</p>`;
+}
+document.getElementById('dessertMenuItems').innerHTML = dessrtItem;
