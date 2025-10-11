e837b7c072f9f4d26d2e98fc20ab1feee234cc4e
Damian Zabagło <damian_zabaglo@epam.com>
2025-10-11 19:41:18 +0200

    add function generateCards, add function shuffle

---
+++ Lab11 - Memory Match Game/colorful_memoty_game.js
@@ -0,0 +1,30 @@
+const colors = ['red', 'blue', 'green', 'purple', 'orange', 'pink', 'red', 'blue', 'green', 'purple', 'orange', 'pink'];
+let cards = shuffle(colors.concat(colors));
+let selectedCards = [];
+let score = 0;
+let timeLeft = 30;
+let gameInterval;
+
+const startbtn = document.getElementById('startbtn');
+const gameContainer = document.getElementById('game-container');
+const scoreElement = document.getElementById('score');
+const timerElement = document.getElementById('timer');
+
+function generateCards(){
+    for(const color of cards){
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.dataset.color = color;
+        card.textContent = '?';
+        gameContainer.appendChild(card);
+    }
+}
+
+function shuffle(array){
+    for(let i = array.length - 1; i > 0; i--){
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]]= [array[j], array[i]];
+    }
+    return array;
+}
+
